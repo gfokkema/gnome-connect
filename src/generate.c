46e7d3a6d98f6b09805a6809a992bcc9706f2de3
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-01-31 14:21:57 +0100

    Fix indentation

--- src/generate.c
+++ src/generate.c
@@ -1,94 +1,94 @@
-#include "gcrypt.h"
+#include <gcrypt.h>
 
-#define SAFE(call)												\
-{																\
-	int err = call;												\
-	if (err)													\
-	{															\
-		fprintf(stderr, "Error in file '%s' in line %i: %d.\n",	\
-				__FILE__, __LINE__, err);						\
-		exit(EXIT_FAILURE);										\
-	}															\
+#define SAFE(call)                                              \
+{                                                               \
+    int err = call;                                             \
+    if (err)                                                    \
+    {                                                           \
+        fprintf(stderr, "Error in file '%s' in line %i: %d.\n", \
+                __FILE__, __LINE__, err);                       \
+                exit(EXIT_FAILURE);                             \
+    }                                                           \
 }
 
 
 void
 progress (void *cb_data, const char *message,
-		  int printchar, int current, int total)
+          int printchar, int current, int total)
 {
-	printf ("Progress: %d %d\nWhat: %s\n", current, total, message);
+    printf ("Progress: %d %d\nWhat: %s\n", current, total, message);
 }
 
 void
 sexpressioninfo (const gcry_sexp_t *key)
 {
-	int size = gcry_sexp_sprint(*key, GCRYSEXP_FMT_ADVANCED, NULL, 0);
+    int size = gcry_sexp_sprint(*key, GCRYSEXP_FMT_ADVANCED, NULL, 0);
 
-	char* buffer = malloc(size);
-	gcry_sexp_sprint (*key, GCRYSEXP_FMT_ADVANCED, buffer, size);
+    char* buffer = malloc(size);
+    gcry_sexp_sprint (*key, GCRYSEXP_FMT_ADVANCED, buffer, size);
 
-	printf("%s\n", buffer);
+    printf("%s\n", buffer);
 }
 
 void
 generate (gcry_sexp_t *private_key, gcry_sexp_t *public_key) {
-	gcry_sexp_t keyparm, key;
+    gcry_sexp_t keyparm, key;
 
-	SAFE( gcry_sexp_new (&keyparm,
-						 "(genkey\n"
-						 "  (rsa\n"
-						 "    (transient-key)\n"
-						 "    (nbits 4:1024)\n"
-						 "  )\n"
-						 ")", 0, 1) );
-	SAFE( gcry_pk_genkey (&key, keyparm) );
-	gcry_sexp_release (keyparm);
+    SAFE( gcry_sexp_new (&keyparm,
+                         "(genkey\n"
+                         "  (rsa\n"
+                         "    (transient-key)\n"
+                         "    (nbits 4:1024)\n"
+                         "  )\n"
+                         ")", 0, 1) );
+    SAFE( gcry_pk_genkey (&key, keyparm) );
+    gcry_sexp_release (keyparm);
 
-	*public_key  = gcry_sexp_nth(key, 1);
-	*private_key = gcry_sexp_nth(key, 2);
+    *public_key  = gcry_sexp_nth(key, 1);
+    *private_key = gcry_sexp_nth(key, 2);
 }
 
 void
 decrypt (gcry_sexp_t *plain,
-		 const gcry_sexp_t *key, gcry_sexp_t *cipher)
+         const gcry_sexp_t *key, gcry_sexp_t *cipher)
 {
     SAFE( gcry_pk_decrypt(plain, *cipher, *key) );
 }
 
 void
 encrypt (gcry_sexp_t *cipher,
-		 const gcry_sexp_t *key, const char* str)
+         const gcry_sexp_t *key, const char* str)
 {
     gcry_mpi_t msg;
     gcry_sexp_t data;
 
     SAFE( gcry_mpi_scan(&msg, GCRYMPI_FMT_USG, str, strlen(str), NULL) );
     SAFE( gcry_sexp_build(&data, NULL, "(data (flags raw) (value %m))", msg) );
-	SAFE( gcry_pk_encrypt(cipher, data, *key) );
+    SAFE( gcry_pk_encrypt(cipher, data, *key) );
 }
 
 int main()
 {
-	if (!gcry_check_version (GCRYPT_VERSION))
-	{
-		fputs ("libgcrypt version mismatch\n", stderr);
-		return 2;
-	}
-	SAFE( gcry_control (GCRYCTL_DISABLE_SECMEM, 0) );
-	SAFE( gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0) );
-	gcry_set_progress_handler (&progress, NULL);
+    if (!gcry_check_version (GCRYPT_VERSION))
+    {
+        fputs ("libgcrypt version mismatch\n", stderr);
+        return 2;
+    }
+    SAFE( gcry_control (GCRYCTL_DISABLE_SECMEM, 0) );
+    SAFE( gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0) );
+    gcry_set_progress_handler (&progress, NULL);
 
-	gcry_sexp_t private, public;
-	generate(&private, &public);
+    gcry_sexp_t private, public;
+    generate(&private, &public);
 
-	sexpressioninfo(&public);
-	sexpressioninfo(&private);
+    sexpressioninfo(&public);
+    sexpressioninfo(&private);
 
-	gcry_sexp_t cipher;
-	encrypt(&cipher, &public, "test");
-	sexpressioninfo(&cipher);
+    gcry_sexp_t cipher;
+    encrypt(&cipher, &public, "test");
+    sexpressioninfo(&cipher);
 
-	gcry_sexp_t plain;
-	decrypt(&plain, &private, &cipher);
-	sexpressioninfo(&plain);
+    gcry_sexp_t plain;
+    decrypt(&plain, &private, &cipher);
+    sexpressioninfo(&plain);
 }
--- src/notify.c
+++ src/notify.c
@@ -5,25 +5,25 @@
 
 Notify* new_notify()
 {
-	notify_init ("Hello world!");
+    notify_init ("Hello world!");
 
-	Notify* notify = malloc(sizeof(Notify));
-	if (!notify)
-	{
-		exit(EXIT_FAILURE);
-	}
+    Notify* notify = malloc(sizeof(Notify));
+    if (!notify)
+    {
+        exit(EXIT_FAILURE);
+    }
 
-	return notify;
+    return notify;
 }
 
 
 void notify() {
-	NotifyNotification * hello = notify_notification_new ("Hello world", "This is an example notification.", "dialog-information");
-	notify_notification_show (hello, NULL);
-	g_object_unref(G_OBJECT(hello));
+    NotifyNotification * hello = notify_notification_new ("Hello world", "This is an example notification.", "dialog-information");
+    notify_notification_show (hello, NULL);
+    g_object_unref(G_OBJECT(hello));
 }
 
 void destroy_notify()
 {
-	notify_uninit();
+    notify_uninit();
 }
